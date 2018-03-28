27033e6bb0b4fb2c25dbb4cdb20a10e6a8559355
ehsanasgari <asgari@berkeley.edu>
2018-03-28 16:49:51 +0200

    add classifiers / bootstrapping

---
+++ classifier/classical_classifiers.py
@@ -0,0 +1,180 @@
+__author__ = "Ehsaneddin Asgari"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Ehsaneddin Asgari"
+__email__ = "asgari@berkeley.edu or ehsaneddin.asgari@helmholtz-hzi.de"
+__project__ = "LLP - Life Language Processing"
+__website__ = "https://llp.berkeley.edu/"
+
+import sys
+
+sys.path.append('../')
+from sklearn.svm import LinearSVC, SVC
+from classifier.cross_validation import KFoldCrossVal, PredefinedFoldCrossVal
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from utility.file_utility import FileUtility
+import numpy as np
+import codecs
+import math
+import operator
+
+class SVM:
+    '''
+        Support vector machine classifier
+    '''
+    def __init__(self, X, Y, clf_model='LSVM'):
+        if clf_model == 'LSVM':
+            self.model = LinearSVC(C=1.0)#, multi_class='ovr'
+            self.type = 'linear'
+        else:
+            self.model = SVC(C=1.0, kernel='rbf')
+            self.type = 'rbf'
+        self.X = X
+        self.Y = Y
+
+    def tune_and_eval(self, results_file,
+                      params=[{'C': [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1, 0.2, 0.5, 0.01, 0.02, 0.05, 0.001],'penalty':['l1'], "tol": [1e-06,1e-04], 'dual':[False,True], "fit_intercept": [True], 'loss':['l2'] , 'class_weight':['balanced',None]}]):
+        CV = KFoldCrossVal(self.X, self.Y, folds=10)
+        CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_SVM',
+                             n_jobs=50)
+
+    def tune_and_eval_predefined(self, results_file,isolates, folds,params=[{'C': [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1, 0.2, 0.5, 0.01, 0.02, 0.05, 0.001],'penalty':['l1'], "tol": [1e-06,1e-04], 'dual':[False,True], "fit_intercept": [True], 'loss':['l2'] , 'class_weight':['balanced',None]}]):
+        '''
+        Tune, evaluate, extract features (if a list of features are provided)
+        :param results_file:
+        :param params:
+        :param feature_names:
+        :return:
+        '''
+        self.CV = PredefinedFoldCrossVal(self.X, self.Y, isolates, folds)
+        self.CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_SVM',
+                                  n_jobs=20)
+
+class LogRegression:
+    '''
+        LR classifier
+    '''
+    def __init__(self, X, Y):
+        self.model = LogisticRegression(C=1.0)
+        self.X = X
+        self.Y = Y
+
+    def tune_and_eval(self, results_file,
+                      params=[{'C': [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1, 0.2, 0.5, 0.01, 0.02, 0.05, 0.001],'penalty':['l1'], "tol": [1e-06,1e-04], 'dual':[False,True], "fit_intercept": [True],  'class_weight':['balanced',None], 'solver':['liblinear']}]):
+        CV = KFoldCrossVal(self.X, self.Y, folds=10)
+        CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_LR',
+                             n_jobs=20)
+
+    def tune_and_eval_predefined(self, results_file,isolates, folds,params=[{'C': [1000, 500, 200, 100, 50, 20, 10, 5, 2, 1, 0.2, 0.5, 0.01, 0.02, 0.05, 0.001],'penalty':['l1'], "tol": [1e-06,1e-04], 'dual':[False,True], "fit_intercept": [True],  'class_weight':['balanced',None], 'solver':['liblinear']}]):
+        '''
+        Tune, evaluate, extract features (if a list of features are provided)
+        :param results_file:
+        :param params:
+        :param feature_names:
+        :return:
+        '''
+        self.CV = PredefinedFoldCrossVal(self.X, self.Y, isolates, folds)
+        self.CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_LR',
+                                  n_jobs=20)
+
+class RFClassifier:
+    '''
+        Random forest classifier
+    '''
+    def __init__(self, X, Y):
+        self.model = RandomForestClassifier(bootstrap=True, criterion='gini',
+                                            min_samples_split=2, max_features='auto', min_samples_leaf=1,
+                                            n_estimators=1000)
+        self.X = X
+        self.Y = Y
+
+    def tune_and_eval(self, results_file, params=None, feature_names=None):
+        '''
+        Tune, evaluate, extract features (if a list of features are provided)
+        :param results_file:
+        :param params:
+        :param feature_names:
+        :return:
+        '''
+        if params is None:
+            params = [{"n_estimators": [100, 200, 500, 1000],
+                       "criterion": ["entropy"],  # "gini",
+                       'max_features': ['sqrt','auto'],  # 'auto',
+                       'min_samples_split': [2,5,10],  # 2,5,10
+                       'min_samples_leaf': [1], 'class_weight':['balanced',None]}]
+        self.CV = KFoldCrossVal(self.X, self.Y, folds=10)
+        self.CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_RF',
+                                  n_jobs=20)
+        if feature_names is not None:
+            [label_set, conf, best_score_, best_estimator_, cv_results_, best_params_, (y_predicted, Y,label_set )]=FileUtility.load_obj(results_file + '_RF.pickle')
+            self.generate_RF_important_features(best_estimator_,feature_names,results_file,500)
+
+    def tune_and_eval_predefined(self, results_file,isolates, folds, params=None, feature_names=None):
+        '''
+        Tune, evaluate, extract features (if a list of features are provided)
+        :param results_file:
+        :param params:
+        :param feature_names:
+        :return:
+        '''
+        if params is None:
+            params = [{"n_estimators": [100, 200, 500, 1000],
+                       "criterion": ["entropy"],  # "gini",
+                       'max_features': ['sqrt','auto'],  # 'auto',
+                       'min_samples_split': [2,5,10],  # 2,5,10
+                       'min_samples_leaf': [1,2], 'class_weight':['balanced',None]}]
+        self.CV = PredefinedFoldCrossVal(self.X, self.Y, isolates, folds)
+        self.CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_RF',
+                                  n_jobs=20)
+        if feature_names is not None:
+            [label_set, conf, best_score_, best_estimator_, cv_results_, best_params_,(y_predicted, Y,label_set )]=FileUtility.load_obj(results_file + '_RF.pickle')
+            self.generate_RF_important_features(best_estimator_,feature_names,results_file,1000)
+
+    def generate_RF_important_features(self, clf_random_forest,feature_names,results_file, N):
+        file_name=results_file+'RF_features'
+        clf_random_forest.fit(self.X, self.Y)
+        std = np.std([tree.feature_importances_ for tree in clf_random_forest.estimators_],axis=0)
+
+        scores = {feature_names[i]: (s,std[i]) for i, s in enumerate(list(clf_random_forest.feature_importances_)) if not math.isnan(s) }
+        scores = sorted(scores.items(), key=operator.itemgetter([1][0]),reverse=True)[0:N]
+        f = codecs.open(file_name,'w')
+        f.write('\t'.join(['feature', 'score', 'std', '#I-out-of-'+str(np.sum(self.Y)), '#O-out-of-'+str(len(self.Y)-np.sum(self.Y))])+'\n')
+        for w, score in scores:
+            feature_array=self.X[:,feature_names.index(w)]
+            pos=[feature_array[idx] for idx, x in enumerate(self.Y) if x==1]
+            neg=[feature_array[idx] for idx, x in enumerate(self.Y) if x==0]
+            f.write('\t'.join([str(w), str(score[0]), str(score[1]), str(np.sum(pos)), str(np.sum(neg))])+'\n')
+        f.close()
+
+class KNN:
+    '''
+        K-nearest neighbor classifier
+    '''
+    def __init__(self, X, Y):
+        self.model = KNeighborsClassifier(n_neighbors=3)
+        self.X = X
+        self.Y = Y
+
+    def tune_and_eval(self, results_file, params=None):
+        if params is None:
+            params = [{"n_neighbors": [1,2,3,4,5,6,7,8,9,10,15,20],'weights':['uniform','distance']}]
+        self.CV = KFoldCrossVal(self.X, self.Y, folds=10)
+        self.CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_KNN',
+                                  n_jobs=30)
+
+
+    def tune_and_eval_predefined(self, results_file,isolates, folds, params=None):
+        '''
+        Tune, evaluate, extract features (if a list of features are provided)
+        :param results_file:
+        :param params:
+        :param feature_names:
+        :return:
+        '''
+        if params is None:
+             params = [{"n_neighbors": [1,2,3,4,5,6,7,8,9,10,15,20],'weights':['uniform','distance']}]
+        self.CV = PredefinedFoldCrossVal(self.X, self.Y, isolates, folds)
+        self.CV.tune_and_evaluate(self.model, parameters=params, score='f1_macro', file_name=results_file + '_KNN',
+                                  n_jobs=20)
---
+++ classifier/cross_validation.py
@@ -0,0 +1,248 @@
+__author__ = "Ehsaneddin Asgari"
+__license__ = "GPL"
+__version__ = "1.0.0"
+__maintainer__ = "Ehsaneddin Asgari"
+__email__ = "asgari@berkeley.edu or ehsaneddin.asgari@helmholtz-hzi.de"
+__project__ = "LLP - Life Language Processing"
+__website__ = "https://llp.berkeley.edu/"
+
+
+import sys
+
+sys.path.append('../')
+from sklearn.model_selection import GridSearchCV, StratifiedKFold, cross_val_predict, cross_val_score
+from utility.file_utility import FileUtility
+from sklearn.metrics import confusion_matrix, roc_auc_score
+from sklearn.metrics.classification import precision_recall_fscore_support
+from sklearn.metrics.scorer import make_scorer
+
+
+class CrossValidator(object):
+    '''
+     The Abstract Cross-Validator
+    '''
+
+    def __init__(self, X, Y):
+        self.X = X
+        self.Y = Y
+        self.scoring = {#'auc_score_macro': make_scorer(self.roc_auc_macro),
+                        #'auc_score_micro': make_scorer(self.roc_auc_micro),
+                        'accuracy': 'accuracy',
+                        'scores_p_1': 'precision',
+                        'scores_r_1': 'recall',
+                        'scores_f_1_1': 'f1',
+                        'scores_p_0': make_scorer(self.precision_0),
+                        'scores_r_0': make_scorer(self.recall_0),
+                        'scores_f_1_0': make_scorer(self.f1_0),
+                        'tnr': make_scorer(self.TNR),
+                        'precision_micro': 'precision_micro',
+                        'precision_macro': 'precision_macro', 'recall_macro': 'recall_macro',
+                        'recall_micro': 'recall_micro', 'f1_macro': 'f1_macro', 'f1_micro': 'f1_micro'}
+
+    def TNR(self, y_true, y_pred):
+        '''
+        :param y_true:
+        :param y_pred:
+        :return: True-negative rate
+        '''
+        TP = 0
+        FP = 0
+        TN = 0
+        FN = 0
+        for i in range(len(y_pred)):
+            if (y_true[i] == y_pred[i]) and y_pred[i] == 1:
+                TP += 1
+            if y_pred[i] == 1 and (y_true[i] != y_pred[i]):
+                FP += 1
+            if y_true[i] == y_pred[i] and y_pred[i] == 0:
+                TN += 1
+            if y_pred[i] == 0 and y_true[i] != y_pred[i]:
+                FN += 1
+        return float(TN / (TN + FP))
+
+    def roc_auc_macro(self, y_true, y_score):
+        return roc_auc_score(y_true, y_score, average="macro")
+
+    def roc_auc_micro(self, y_true, y_score):
+        return roc_auc_score(y_true, y_score, average="micro")
+
+    def precision_0(self, y_true, y_pred, labels=None, average='binary', sample_weight=None):
+        '''
+        :param y_true:
+        :param y_pred:
+        :param labels:
+        :param average:
+        :param sample_weight:
+        :return: calculate prec for neg class
+        '''
+        p, _, _, _ = precision_recall_fscore_support(y_true, y_pred,
+                                                     beta=1,
+                                                     labels=labels,
+                                                     pos_label=0,
+                                                     average=average,
+                                                     warn_for=('f-score',),
+                                                     sample_weight=sample_weight)
+        return p
+
+    def recall_0(self, y_true, y_pred, labels=None, average='binary', sample_weight=None):
+        '''
+        :param y_true:
+        :param y_pred:
+        :param labels:
+        :param average:
+        :param sample_weight:
+        :return: calculate recall for neg class
+        '''
+        _, r, _, _ = precision_recall_fscore_support(y_true, y_pred,
+                                                     beta=1,
+                                                     labels=labels,
+                                                     pos_label=0,
+                                                     average=average,
+                                                     warn_for=('f-score',),
+                                                     sample_weight=sample_weight)
+        return r
+
+    def f1_0(self, y_true, y_pred, labels=None, average='binary', sample_weight=None):
+        '''
+        :param y_true:
+        :param y_pred:
+        :param labels:
+        :param average:
+        :param sample_weight:
+        :return: calculate f1 for neg class
+        '''
+        _, _, f, _ = precision_recall_fscore_support(y_true, y_pred,
+                                                     beta=1,
+                                                     labels=labels,
+                                                     pos_label=0,
+                                                     average=average,
+                                                     warn_for=('f-score',),
+                                                     sample_weight=sample_weight)
+        return  f
+
+
+class KFoldCrossVal(CrossValidator):
+    '''
+        K-fold cross-validation tuning and evaluation
+    '''
+
+    def __init__(self, X, Y, folds=10, random_state=1):
+        '''
+        :param X:
+        :param Y:
+        :param folds:
+        :param random_state:
+        '''
+        CrossValidator.__init__(self, X, Y)
+        self.cv = StratifiedKFold(n_splits=folds, shuffle=True, random_state=random_state)
+        self.X = X
+        self.Y = Y
+
+    def tune_and_evaluate(self, estimator, parameters, score='f1_macro', n_jobs=-1, file_name='results'):
+        '''
+        :param estimator:
+        :param parameters:p
+        :param score:
+        :param n_jobs:
+        :param file_name: directory/tuning/classifier/features/
+        :return:
+        '''
+        # greed_search
+        self.greed_search = GridSearchCV(estimator=estimator, param_grid=parameters, cv=self.cv, scoring=self.scoring,
+                                         refit=score, error_score=0, n_jobs=n_jobs)
+
+        label_set = list(set(self.Y))
+        # fitting
+        self.greed_search.fit(X=self.X, y=self.Y)
+        y_predicted = cross_val_predict(self.greed_search.best_estimator_, self.X, self.Y,  cv=self.cv)
+        conf = confusion_matrix(self.Y, y_predicted, labels=label_set)
+        # save in file
+        FileUtility.save_obj(file_name,
+                             [label_set, conf, self.greed_search.best_score_, self.greed_search.best_estimator_,
+                              self.greed_search.cv_results_, self.greed_search.best_params_,  (y_predicted, self.Y,label_set )])
+
+
+class NestedCrossVal(CrossValidator):
+    '''
+    Nested cross-validation
+    '''
+
+    def __init__(self, X, Y, inner_folds=10, outer_folds=10, random_state=1, opt_f1_class=0):
+        '''
+        :param X:
+        :param Y:
+        :param inner_folds:
+        :param outer_folds:
+        :param random_state:
+        :param opt_f1_class:
+        '''
+        CrossValidator.__init__(self, X, Y, opt_f1_class=opt_f1_class)
+        self.inner_cv = StratifiedKFold(n_splits=inner_folds, shuffle=True, random_state=random_state)
+        self.outer_cv = StratifiedKFold(n_splits=outer_folds, shuffle=True, random_state=random_state)
+
+    def tune_and_evaluate(self, estimator, parameters, score='f1_macro', file_name='results'):
+        '''
+        :param estimator:
+        :param parameters:
+        :param score:
+        :param file_name: directory/tuning/classifier/features/
+        :return:
+        '''
+        # inner cross_validation
+        self.greed_search = GridSearchCV(estimator=estimator, param_grid=parameters, cv=self.inner_cv,
+                                         scoring=self.scoring, refit=score, error_score=0)
+        # Nested CV with parameter optimization
+        self.nested_score = cross_val_score(self.greed_search, X=self.X, y=self.Y, cv=self.outer_cv)
+
+        # saving
+        FileUtility.save_obj([self.greed_search, self.nested_score], file_name)
+
+
+class PredefinedFoldCrossVal(CrossValidator):
+    '''
+        Predefined folds
+    '''
+
+    def __init__(self, X, Y, isolate_list, fold_file):
+        '''
+        :param X:
+        :param Y:
+        :param folds:
+        :param random_state:
+        '''
+        CrossValidator.__init__(self, X, Y)
+
+        map_to_idx = {isolate: idx for idx, isolate in enumerate(isolate_list)}
+        splits = [[map_to_idx[item] for item in fold_list.split() if item in map_to_idx] for fold_list in
+                  FileUtility.load_list(fold_file)]
+        new_splits = []
+        for i in range(len(splits)):
+            train = [j for i in splits[:i] + splits[i + 1:] for j in i]
+            test = splits[i]
+            new_splits.append([train, test])
+        self.cv = new_splits
+        self.X = X
+        self.Y = Y
+
+    def tune_and_evaluate(self, estimator, parameters, score='f1_macro', n_jobs=-1, file_name='results'):
+        '''
+        :param estimator:
+        :param parameters:p
+        :param score:
+        :param n_jobs:
+        :param file_name: directory/tuning/classifier/features/
+        :return:
+        '''
+        # greed_search
+        self.greed_search = GridSearchCV(estimator=estimator, param_grid=parameters, cv=self.cv, scoring=self.scoring,
+                                         refit=score, error_score=0, n_jobs=n_jobs)
+
+        label_set = list(set(self.Y))
+        # fitting
+        self.greed_search.fit(X=self.X, y=self.Y)
+        y_predicted = cross_val_predict(self.greed_search.best_estimator_, self.X, self.Y,  cv=self.cv)
+        conf = confusion_matrix(self.Y, y_predicted, labels=label_set)
+        # save in file
+        FileUtility.save_obj(file_name,
+                             [label_set, conf, self.greed_search.best_score_, self.greed_search.best_estimator_,
+                              self.greed_search.cv_results_, self.greed_search.best_params_, (y_predicted, self.Y,label_set )])
