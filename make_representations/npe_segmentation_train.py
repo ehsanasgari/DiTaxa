1527ce7bace86fc4187619ee6a067da4ef954bc2
ehsanasgari <asgari@berkeley.edu>
2018-09-05 16:39:00 +0200

    fix sentencepiece error

--- make_representations/npe_segmentation_train.py
+++ make_representations/npe_segmentation_train.py
@@ -60,7 +60,7 @@ class NPESegmentTrainMetagenomics:
         if backend == 'Sentencepiece':
             FileUtility.save_list('tmp/tmp_txt', corpus)
             spm.SentencePieceTrainer.Train(
-                '--input=tmp/tmp_txt --model_prefix=' + output_dir + ' --add_dummy_prefix false --max_sentencepiece_length=512 --model_type=bpe --mining_sentence_size=5000000 --input_sentence_size=10000000 --vocab_size=50000')
+                '--input=tmp/tmp_txt --model_prefix=' + output_dir + ' --add_dummy_prefix=0 --max_sentencepiece_length=512 --model_type=bpe --mining_sentence_size=5000000 --input_sentence_size=10000000 --vocab_size=50000')
             FileUtility.save_list('tmp/tmp_txt', corpus[0:10])
         elif backend == 'normalbpe':
             train_npe(corpus, output_dir, vocab_size, output_dir + '_freq')
