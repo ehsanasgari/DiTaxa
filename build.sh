24eac2d3014726e5e8614b57c6a7aaaadb2790fc
Ehsaneddin Asgari <asgari@berkeley.edu>
2018-09-19 16:18:58 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,3 @@
-#!/usr/bin/env bash
-
 if [ "$(uname)" == "Darwin" ]; then
     curl -o blast.tar.gz 'https://ftp.ncbi.nlm.nih.gov/blast/executables/blast+/2.7.1/ncbi-blast-2.7.1+-x64-macosx.tar.gz'
     gunzip -c blast.tar.gz | tar xopf -
