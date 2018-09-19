e691310e12c39ed292a8ae05de77ba37d9da434e
Ehsaneddin Asgari <asgari@berkeley.edu>
2018-09-19 16:19:23 +0200

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,1 @@
-#!/usr/bin/env bash
-
 python3 ditaxa.py --indir dataset/periodontal/ --fast2label dataset/periodontal/mapping.txt --ext fastq --outdir results_dental/ --dbname periodontal --cores 4 --phenomap diseased:1,healthy:0 --phenoname DvsH --override 0 --heatmap periodontal:healthy
